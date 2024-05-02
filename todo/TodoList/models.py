fb2b6a1c4ec8cf27aa71030a7b2b53e48e885fdb
maxmm <maxmmuskietorz@gmail.com>
2024-05-02 19:38:32 +0100

    commit Task_Details

--- todo/TodoList/models.py
+++ todo/TodoList/models.py
@@ -2,9 +2,14 @@ from django.db import models
 
 # Create your models here.
 class Task(models.Model):
+    STATUS = [("pending", "Pending"), ("completed", "Completed")]
+    TYPES = [("personal", "Personal"), ("work", "Work"), ("study", "Study")]
+    PRIORITY = [("urgent", "Urgent"), ("normal", "Normal"), ("low", "Low")]
     def __str__(self):
         return self.title
 
     title = models.CharField(max_length=1000, null=True, blank=True)
     description = models.TextField(max_length=500)
-    status = models.CharField(max_length=20, choices=[('pending', 'Pending'), ('completed', 'Completed')], default="pending")
+    status = models.CharField(max_length=20, choices= STATUS, default="pending")
+    type = models.CharField(max_length=20, choices= TYPES, default="personal ")
+    priority = models.CharField(max_length=20, choices= PRIORITY, default="normal")
