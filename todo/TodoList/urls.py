aeedd0c841004db1d931c645fce02194a75e7b8b
maxmm <maxmmuskietorz@gmail.com>
2024-05-02 19:44:39 +0100

    commit new_urls

--- todo/TodoList/urls.py
+++ todo/TodoList/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path("" ,views.TaskList, name="tasklist"),
     path("create/", views.CreateTask, name="create_task"),
+    path("complete/<int:task_id>/" ,views.CompleteTask, name="completetask"),
+    path("delete/<int:task_id>/", views.DeleteTask, name="deletetask"),
     ]
