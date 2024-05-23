e5de3145e012c93c6d221a0cadb306025494761e
ricardorosa <rjrosa@outlook.com>
2024-05-23 19:26:03 +0100

    commit comments

--- todo/TodoList/static/TodoList/JS/main.js
+++ todo/TodoList/static/TodoList/JS/main.js
@@ -1,4 +1,8 @@
+
+// Get all delete buttons
 const deleteButtons = document.querySelectAll('.task-delete-btn')
+
+// Attach event listener to each delete button
 deleteButtons.forEach(button=>{
 button.addEventListener("click", function(event){
 
