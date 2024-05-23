560fb0fd2313bcb1fb3f33650df03a2a8a45d922
maxmm <maxmmuskietorz@gmail.com>
2024-05-23 20:03:31 +0100

    commit python

--- todo/TodoList/static/TodoList/JS/main.js
+++ todo/TodoList/static/TodoList/JS/main.js
@@ -5,6 +5,44 @@ const deleteButtons = document.querySelectAll('.task-delete-btn')
 // Attach event listener to each delete button
 deleteButtons.forEach(button=>{
 button.addEventListener("click", function(event){
+        // Prevent the dfault form submisson
+        event.preventDefault();
 
+        // Prompt the user for conformation
+        const confirmed = confirm("You are about to delete this task, are you sure you want to continue?");
+
+        // If user confirms deletion, submit the form
+        if (confirmed) {
+            // Find the form associated with the delete button
+            const form = button.closest('form');
+
+            // Submit the form
+            form.submit();
+        }
 });
 });
+
+// Get all save buttons
+const deleteButtons = document.querySelectAll('.task-save-btn')
+
+// Attach event listener to each save button
+saveButtons.forEach(button=>{
+button.addEventListener("click", function(event){
+        // Prevent the dfault form submisson
+        event.preventDefault();
+
+        // Prompt the user for conformation
+        const confirmed = confirm("Are you certain you have completed your task");
+
+        // If user confirms deletion, submit the form
+        if (confirmed) {
+            // Find the form associated with the save button
+            const form = button.closest('form');
+
+            // Submit the form
+            form.submit();
+        }
+});
+});
+
+
