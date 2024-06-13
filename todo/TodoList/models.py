639ef6aa463530bb014d7823a9a758e2cebf8166
ricardorosa <rjrosa@outlook.com>
2024-06-13 19:54:07 +0100

    Resolve Task model

--- todo/TodoList/models.py
+++ todo/TodoList/models.py
@@ -1,16 +1,31 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 class Task(models.Model):
-    STATUS = [("pending", "Pending"), ("completed", "Completed")]
-    TYPES = [("personal", "Personal"), ("work", "Work"), ("study", "Study")]
-    PRIORITY = [("urgent", "Urgent"), ("normal", "Normal"), ("low", "Low")]
+    STATUS_CHOICES = [
+        ('pending', 'Pending'),
+        ('completed', 'Completed'),
+    ]
+
+    TYPE_CHOICES = [
+        ('personal', 'Personal'),
+        ('work', 'Work'),
+        ('study', 'Study'),
+    ]
+
+    CATEGORY_CHOICES = [
+        ('urgent', 'Urgent'),
+        ('normal', 'Normal'),
+        ('low', 'Low'),
+    ]
+
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
+    task_type = models.CharField(max_length=20, choices=TYPE_CHOICES)
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES, default='normal')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+
     def __str__(self):
         return self.title
 
-    title = models.CharField(max_length=1000, null=True, blank=True)
-    description = models.TextField(max_length=500)
-    status = models.CharField(max_length=20, choices= STATUS, default="pending")
-    type = models.CharField(max_length=20, choices= TYPES, default="personal ")
-    priority = models.CharField(max_length=20, choices= PRIORITY, default="normal")
-    # add a user field to the module
